2749a5822075da105b8123131d3c7a3b0e767d2d
JoseDavidPonce <60592092+JoseDavidPonce@users.noreply.github.com>
2020-05-29 18:06:30 -0600

    Avances de configuración y desbloqueo

--- Debug/main.d
+++ Debug/main.d
@@ -12,11 +12,16 @@ main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine
 main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h
 main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h
 main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdbool.h
-main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
-main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/TM4C123GH6PM.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
 
@@ -32,10 +37,15 @@ C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h:
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_stdint.h: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_stdint.h: 
 C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdbool.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/string.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/TM4C123GH6PM.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
--- main.c
+++ main.c
@@ -10,17 +10,24 @@ el Ing. Pablo Mazariegos.
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/tm4c123gh6pm.h"
-#include "inc/hw_memmap.h"
+#include <string.h>
+#include "inc/TM4C123GH6PM.h"
+#include "inc/hw_gpio.h"
 #include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
 #include "inc/hw_ints.h"
+#include "driverlib/fpu.h"
+#include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
-//#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
 #include "driverlib/gpio.h"
-//#include "driverlib/timer.h"
 #include "driverlib/uart.h"
 
+
+void enviarColorCorrespondiente(void);
+
 bool pulsado = 0;
+bool pulsado2 = 0;
 uint8_t color = 0;
 uint8_t leds = 0;
 
@@ -28,6 +35,9 @@ int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
     //Se configura el reloj para que tenga una frecuencia de 40MHz
+    //Es necesario desbloquear el botón SW2
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R = 0X00000001;
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     //Se activa el periférico del puerto F
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){
@@ -36,14 +46,30 @@ int main(void)
     //Luego se activan como outputs los pines 1 2 y 3 (LEDS del RGB)
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
     //Es necesario configurar el pin del botón SW1 que es el pin 4 del puerto F como input
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4| GPIO_PIN_0);
     //Acá se configura la resistencia weak pull up para el pin del botón
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4 | GPIO_PIN_0, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
+
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    GPIOPinConfigure(GPIO_PC4_U1RX);
+    GPIOPinConfigure(GPIO_PC5_U1TX);
+    GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+    UARTConfigSetExpClk(UART1_BASE, 16000000, 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    UARTEnable(UART1_BASE);
+    //comando AT
+    //delay
 
 	while(1){
+	    if (!GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0)){
+	        pulsado2 = 1;
+	    }
 	    if (!GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)){
             pulsado = 1;
 	    }
+
 	    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)){
 	        SysCtlDelay(100);
 	        if (pulsado == 1){
@@ -52,8 +78,23 @@ int main(void)
 	            pulsado = 0;
 	        }
 	    }
+	    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0)){
+	        SysCtlDelay(100);
+	        if (pulsado2 == 1){
+	            enviarColorCorrespopndiente();
+	            pulsado2 = 0;
+	        }
+
+	    }
 	    leds= color<<1;
 	    GPIOPinWrite(GPIO_PORTF_BASE, 0x0E, leds);
 
 	}
+}
+
+void enviarColorCorrespopndiente(void){
+    switch(color){
+    case 1:
+        //Enviar
+    }
 }
